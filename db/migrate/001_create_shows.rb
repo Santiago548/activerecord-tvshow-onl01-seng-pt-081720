e84b800504e29c69f8a092c9ef07f6172ad16d67
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-30 17:07:09 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,4 +1,8 @@
 class CreateShows < ActiveRecord::Migration[5.2]
-  create_table
-
+  create_table do |t|
+    t.string :name
+    t.string :network
+    t.string :day
+    t.integer :rating
+  end
 end
