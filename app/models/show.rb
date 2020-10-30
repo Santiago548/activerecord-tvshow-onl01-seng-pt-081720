4c06df1fa67f7bd3554fdaae7702dbc4a03e2661
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-30 17:30:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,25 +1,25 @@
 class Show < ActiveRecord::Base
-  def highest_rating
+  def self.highest_rating
     #Show.
   end
 
-  def most_popular_show
+  def self.most_popular_show
 
   end
 
-  def lowest_rating
+  def self.lowest_rating
 
   end
 
-  def least_popular_show
+  def self.least_popular_show
 
   end
 
-  def ratings_sum
+  def self.ratings_sum
 
   end
 
-  def popular_shows
+  def self.popular_shows
 
   end
 
